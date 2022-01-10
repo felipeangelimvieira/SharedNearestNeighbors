2d5dddc41caf2bf2bc9f508ce542c80b10f86ae3
Felipe Angelim <felipeangelim@gmail.com>
2022-01-09 21:30:45 -0300

    Remove sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,7 @@ import shared_nearest_neighbors
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'nbsphinx',
-              "pydata_sphinx_theme", "sphinx.ext.napoleon"]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'nbsphinx', "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
